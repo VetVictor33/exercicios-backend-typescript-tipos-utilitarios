c742c653b3fc328c8058080b088875a0ab7c2a40
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-23 11:17:09 -0300

    done 01

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -5,7 +5,29 @@ type Usuario = {
     rg: string
     dataNacimento?: string
 }
+type UsuarioSemRg = Omit<Usuario, 'rg'>
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
     return info;
 }
+
+const cadastrarUsuarioSemRg = (info: UsuarioSemRg) => {
+    return info
+}
+
+const meninoDoRg: Usuario = {
+    nome: 'Lukinha',
+    email: 'lukinha@email.com',
+    cpf: '12345678910',
+    rg: '09876543210',
+    dataNacimento: '01/08/1992 8:00 AM'
+}
+
+const meninoSemRg: UsuarioSemRg = {
+    nome: 'Lukinha',
+    email: 'lukinha@email.com',
+    cpf: '12345678910',
+    dataNacimento: '01/08/1992 8:00'
+}
+
+console.log(cadastrarUsuário(meninoDoRg), '\n', cadastrarUsuarioSemRg(meninoSemRg))
