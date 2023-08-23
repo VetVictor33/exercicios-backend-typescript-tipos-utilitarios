02ba837a9bb5258b7eb6a25113e65fc0696352d9
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-23 11:57:21 -0300

    done 03

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -6,10 +6,18 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
+type ConnReturn = {
+    driver: string,
+    url: string
+}
+
+const conexao = (dados: Conn): Readonly<ConnReturn> => {
     const { username, password, host, port, dbname } = dados;
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+const connection = conexao({ username: 'x', password: 'x2', 'host': 'x3', dbname: 'x4', port: 'x5' })
+connection.url = 's'
