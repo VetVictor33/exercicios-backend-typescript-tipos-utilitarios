5fec9b376ef2106fd768400d2123676369da3758
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-23 11:48:39 -0300

    done 02

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,16 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type CarrinhoRefatorado = Omit<Carrinho, 'tipoTransacao'> & {
+    tipoTransacao: Lowercase<Carrinho['tipoTransacao']>,
+    enderoco: Endereco
+}
